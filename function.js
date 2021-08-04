db38e81ca3069004897824506284fedc1711b214
TakiKauser <takikauserbadhon@gmail.com>
2021-08-04 08:13:49 +0600

    function

---
+++ function.js
@@ -0,0 +1,4 @@
+function addition(){
+    console.log("Area to write code.");
+}
+addition();
---
+++ parameter.js
@@ -0,0 +1,11 @@
+function addition(a, b){
+    var c = a + b;
+    // console.log("total: ", c, "taka");
+
+    return c;
+}
+
+var x = 69;
+var y = 43;
+var z = addition(x, y);
+console.log("total: ", z, "taka");
