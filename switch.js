9b6f061eadd634d2e12e0c5cd96b16c254aa6ccd
TakiKauser <takikauserbadhon@gmail.com>
2021-08-04 08:53:57 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+var color = "white";
+
+switch(color){
+    case "blue":
+        console.log("Blue");
+        break;
+    case "red":
+        console.log("Red");
+        break;
+    case "green":
+        console.log("Green");
+        break;
+    case "white":
+        console.log("White");
+        break;
+    default:
+        console.log("You're too much confusing!");  
+}
